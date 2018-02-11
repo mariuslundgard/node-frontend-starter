3877aaf0498fbb1f18d945a2d0ae583b5752c0b6
Marius Lundgård <studio@mariuslundgard.com>
2018-02-11 09:45:16 +0100

    Add default error handling

--- src/app/server.js
+++ src/app/server.js
@@ -15,18 +15,23 @@ import type {Config} from '../types'
 export function create (config: Config) {
   const router = Router()
 
-  router.get('/', async (req: $Request, res: $Response) =>
-    res.send(
-      layout({
-        title: 'App',
-        head: `<link rel="stylesheet" href="/${config.manifest['app.css']}">`,
-        body: [
-          `<div id="${style.root}">${render(<Root />)}</div>`,
-          `<script src="/${config.manifest['app.js']}"></script>`
-        ].join('')
-      })
-    )
-  )
+  router.get('/', async (req: $Request, res: $Response) => {
+    try {
+      res.send(
+        layout({
+          title: 'App',
+          head: `<link rel="stylesheet" href="/${config.manifest['app.css']}">`,
+          body: [
+            `<div id="${style.root}">${render(<Root />)}</div>`,
+            `<script src="/${config.manifest['app.js']}"></script>`
+          ].join('')
+        })
+      )
+    } catch (err) {
+      res.status(500)
+      res.send(err.stack)
+    }
+  })
 
   return router
 }
--- src/frame/server.js
+++ src/frame/server.js
@@ -15,18 +15,25 @@ import type {Config} from '../types'
 export function create (config: Config) {
   const router = Router()
 
-  router.get('/', async (req: $Request, res: $Response) =>
-    res.send(
-      layout({
-        title: 'Frame',
-        head: `<link rel="stylesheet" href="/${config.manifest['frame.css']}">`,
-        body: [
-          `<div id="${style.root}">${render(<Root />)}</div>`,
-          `<script src="/${config.manifest['frame.js']}"></script>`
-        ].join('')
-      })
-    )
-  )
+  router.get('/', async (req: $Request, res: $Response) => {
+    try {
+      res.send(
+        layout({
+          title: 'Frame',
+          head: `<link rel="stylesheet" href="/${
+            config.manifest['frame.css']
+          }">`,
+          body: [
+            `<div id="${style.root}">${render(<Root />)}</div>`,
+            `<script src="/${config.manifest['frame.js']}"></script>`
+          ].join('')
+        })
+      )
+    } catch (err) {
+      res.status(500)
+      res.send(err.stack)
+    }
+  })
 
   return router
 }
