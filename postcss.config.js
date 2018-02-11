0f9ec4aa97ce712782a0b85931965c9385e35f8c
Marius Lundgård <studio@mariuslundgard.com>
2018-02-11 13:49:27 +0100

    Fix import order

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 'use strict'
 
+const autoprefixer = require('autoprefixer')
 const postcssImport = require('postcss-import')
 const postcssCustomProperties = require('postcss-custom-properties')
-const autoprefixer = require('autoprefixer')
 const postCssNesting = require('postcss-nesting')
 
 module.exports = {
